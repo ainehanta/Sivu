e7a416daa013324c6e2f280597a2cbd4c18aa3fd
ainehanta <ainehanta@hotmail.co.jp>
2014-01-30 00:21:41 +0900

    Add dpi setting

--- bin/sivu.rb
+++ bin/sivu.rb
@@ -11,5 +11,6 @@ kit = PDFKit.new(html,
                  :margin_top=>'0in',
                  :margin_right=>'0in',
                  :margin_left=>'0in',
-                 :margin_bottom=>'0in')
+                 :margin_bottom=>'0in',
+                 :dpi=>'300')
 kit.to_file("article.pdf")
