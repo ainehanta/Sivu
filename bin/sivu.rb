e0c91c3eca9a75541d5495eae1ba34e1179ac214
ainehanta <ainehanta@hotmail.co.jp>
2014-01-30 17:34:55 +0900

    Add Page size setting and margins

--- bin/sivu.rb
+++ bin/sivu.rb
@@ -1,8 +1,15 @@
 require 'pdfkit'
 
+#config margin, dpi and page size
+
 html = File.open("article/index.html") do |f|
   f.read
 end
 
-kit = PDFKit.new(html)
+kit = PDFKit.new(html,
+                 :page_size=>'A4',
+                 :margin_top=>'0in',
+                 :margin_right=>'0in',
+                 :margin_left=>'0in',
+                 :margin_bottom=>'0in')
 kit.to_file("article.pdf")
